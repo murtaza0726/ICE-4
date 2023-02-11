48ba0a0cea84c9f221d95aa35fb18a548916d946
Murtaza0726 <murtazanaqvi0726@yahoo.com>
2023-02-11 16:43:50 -0500

    final version of mail pilot

---
+++ Managers/ScoreManager.swift
@@ -0,0 +1,16 @@
+//
+//  ScoreManager.swift
+//  ICE_3
+//
+//  Created by Murtaza Haider Naqvi on 2023-02-11.
+//
+
+import Foundation
+import SpriteKit
+import UIKit
+
+class ScoreManager
+{
+    public static var Score: Int = 0
+    public static var Lives: Int = 5
+}
